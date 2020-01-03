325032cf3c31b7c084a9492061ee039af930b37b
Haksell <Haksell@Haksell.local>
2020-01-03 03:09:44 +0100

    Test new ssh key and add Chapter 05 c13.py

---
+++ C05ArrayBasedSequences/Creativity/c13.py
@@ -0,0 +1,22 @@
+# It always assumes the size of the list will be constant
+# but switches to dynamic mode (with constant 9/8) when an item is appended to the list
+
+import sys
+
+
+def experiment(begin_size=0):
+    data = [None] * begin_size
+    last_size = None
+    while len(data) < 1000:
+        size = sys.getsizeof(data)
+        if size != last_size:
+            print(f"Length : {len(data):>4} | Size in bytes : {size:>5} | Byte per element : {size/len(data):.3f}")
+            last_size = size
+        data.append(None)
+
+
+if __name__ == "__main__":
+    for i in range(1, 1000, 42):
+        print()
+        print(i)
+        experiment(i)
